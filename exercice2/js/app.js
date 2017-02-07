cb65a63b43e4421448b61bf962d4560123d7b368
HireDream <p.maxdu76@gmail.com>
2017-02-07 10:56:37 +0100

    Suppression fichiers inutiles

--- exercice2/js/app.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by PICOT Maxence on 03/02/2017.
- */
-
-angular.module("ServiceApp",[]);
--- projet1/js/noteApp.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by PICOT Maxence on 27/01/2017.
- */
-angular.module("NoteApp",['ngCookies']);
