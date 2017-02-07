843cea05da8b84b775ad57846ceb94e922e2949d
HireDream <p.maxdu76@gmail.com>
2017-02-07 10:58:13 +0100

    TD2 Exo2 - Premier commit, mise en place de l'application

---
+++ td2/exercice2/js/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by PICOT Maxence on 07/02/2017.
+ */
+
+angular.module("ContactApp",[]);
---
+++ td2/exercice2/js/controller.js
@@ -0,0 +1,8 @@
+/**
+ * Created by PICOT Maxence on 07/02/2017.
+ */
+
+angular.module("ContactApp").controller("ContactController", function () {
+    var self = this;
+
+});
