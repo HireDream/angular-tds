071dc165bbc49cd50188566b9317d97639e14dd1
HireDream <p.maxdu76@gmail.com>
2017-02-07 19:04:44 +0100

    TD2 Exo2 - Affichage et modification des contacts

--- td2/exercice2/js/controller.js
+++ td2/exercice2/js/controller.js
@@ -2,7 +2,48 @@
  * Created by PICOT Maxence on 07/02/2017.
  */
 
-angular.module("ContactApp").controller("ContactController", function () {
+angular.module("ContactApp").controller("ContactController",["$http", function ($http) {
     var self = this;
 
-});
+    this.contacts = [];
+    this.contact;
+    this.tmpContact;
+    this.operation;
+    this.edit = false;
+    this.add = false;
+
+    $http.get("json/contacts.json").then(successCallBack);
+    function successCallBack(response) {
+        self.contacts = response.data;
+    }
+
+    this.toUpdate = function($contact) {
+        self.add = false;
+        self.edit = true;
+        self.contact = $contact;
+        self.tmpContact = Object.assign({}, self.contact);
+    }
+
+    this.toAdd = function () {
+        self.add = true;
+        self.edit = false;
+    }
+
+    this.add = function () {
+
+    }
+
+    this.update = function () {
+        var index = self.contacts.indexOf(self.contact);
+        self.contacts[index] = self.tmpContact;
+        self.edit = false;
+    }
+
+    this.delete = function ($contact) {
+        var index = self.contacts.indexOf($contact);
+        self.contacts.splice(index, 1);
+    }
+
+
+
+}]);
